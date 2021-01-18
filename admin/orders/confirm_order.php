a80932f37ad745b740d5c9dfa7de20389b9786ed
Rizqy Ep <rizqyepr@gmail.com>
2021-01-18 23:46:59 +0700

    redirect url fix

--- admin/orders/confirm_order.php
+++ admin/orders/confirm_order.php
@@ -5,10 +5,10 @@ require_once "../../middleware/Admin.php";
 require_once "../../models/Database.php";
 if (Authenticate::isAuthenticated()) {
     if (!Admin::isAdmin($_SESSION['user'])) {
-        header('Location: http://localhost/CodeWear');
+        header('Location: /codewear');
     }
 } else {
-    header('Location: http://localhost/CodeWear');
+    header('Location: /codewear');
 }
 
 $order_id = $_GET['id'];
@@ -17,9 +17,9 @@ $sql = "UPDATE orders SET status = 'On Delivery' WHERE id = '$order_id'";
 if ($conn->query($sql)) {
     $_SESSION['flash_success']['message'] = 'Order is confirmed!';
     $_SESSION['flash_success']['title'] = 'Success';
-    header('Location: http://localhost/CodeWear/admin/orders');
+    header('Location: /codewear/admin/orders');
 } else {
     $_SESSION['flash_fail']['message'] = 'Order not confirmed';
     $_SESSION['flash_fail']['title'] = 'Failed';
-    header('Location: http://localhost/CodeWear/admin/orders');
+    header('Location: /codewear/admin/orders');
 }
